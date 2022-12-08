da660bb33cad22ed78f5460a083e1a1f0d186b5d
Akshat202002 <akshatsamdani23@gmail.com>
2022-12-09 01:28:53 +0530

    added comments for each line

--- game.js
+++ game.js
@@ -1,9 +1,10 @@
-let buttonColors = ["red", "blue", "green", "yellow"]
-let gamePattern = []
-let userClickedPattern = []
-let started = false
-let level = 0
+let buttonColors = ["red", "blue", "green", "yellow"] // We'll need this to store the colors of the buttons
+let gamePattern = [] // We'll need this to store the pattern the game generates
+let userClickedPattern = [] // We'll need this to store the pattern the user clicks
+let started = false // We'll need this to check if the game has started or not
+let level = 0 // We'll need this to keep track of the level
 
+// Use jQuery to detect when a keyboard key has been pressed, when that happens for the first time, call nextSequence().
 $(document).keydown(function () {
     if (!started) {
         $("#level-title").text("Level " + level)
@@ -13,24 +14,46 @@ $(document).keydown(function () {
 })
 
 function nextSequence() {
+    // Increase the level by 1 every time nextSequence() is called.
     level++
+    // Update the h1 with this change in the value of level.
     $("#level-title").text("Level " + level)
+
+    // Once nextSequence() is triggered, reset the userClickedPattern to an empty array ready for the next level.
     userClickedPattern = []
+
+    // Create a new variable called randomNumber to generate a new random number between 0 and 3, and store it in a variable called randomChosenColour.
     let randomNumber = Math.floor(Math.random() * 4)
+
+    // Use the randomNumber to select a random color from the buttonColors array.
     randomChosenColour = buttonColors[randomNumber]
+
+    // Add the new randomChosenColour generated to the end of the gamePattern.
     gamePattern.push(randomChosenColour)
+
+    // Use jQuery to select the button with the same id as the randomChosenColour and animate a flash to the user with a chosen color.
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100)
+
+    // Play sound for the button colour selected.
     playSound(randomChosenColour)
 
 
 }
 
+// Use jQuery to detect when any of the buttons are clicked and trigger a handler function.
 $(".btn").click(function () {
+    // Create a new variable called userChosenColour to store the id of the button that got clicked.
     let userChosenColor = $(this).attr("id")
+
+    // Add the contents of the variable userChosenColour to the end of this new userClickedPattern
     userClickedPattern.push(userChosenColor)
-    console.log(userClickedPattern)
+    // console.log(userClickedPattern)
+
+    // In the same way we played sound in nextSequence(), when a user clicks on a button, the corresponding sound should be played.
     playSound(userChosenColor)
     animatePress(userChosenColor)
+
+    // Call checkAnswer() after a user has clicked and chosen their answer, passing in the index of the last answer in the user's sequence.
     checkAnswer(userClickedPattern.length - 1);
 })
 
@@ -67,6 +90,7 @@ function checkAnswer(currentLevel) {
     }
 }
 
+// We'll need to reset the values of level, gamePattern and started variables after the user gets the sequence wrong.
 function startOver() {
     level = 0
     gamePattern = []
